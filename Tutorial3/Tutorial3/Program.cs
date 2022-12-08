c5e79cbb70b4a5e79398c1b4272e6fdf9c92c4bc
Danielxcott <changwilliam826@gmail.com>
2022-12-08 23:44:48 +0630

    tutorial 3

---
+++ Tutorial3/Tutorial3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Tutorial3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type a number, and then press Enter:");
+            double firstNumber = double.Parse(Console.ReadLine());
+            Console.WriteLine("Type a number, and then press Enter:");
+            double secNumber = double.Parse(Console.ReadLine());
+            Console.WriteLine("a - Add, s - Subtract, m - Multiply, d - Divide");
+            string option = Console.ReadLine();
+            double result;
+            if (option == "a")
+            {
+                result = firstNumber + secNumber;
+                Console.WriteLine(result);
+            }
+            else if (option == "s")
+            {
+                result = firstNumber - secNumber;
+                Console.WriteLine(result);
+            }
+            else if (option == "m")
+            {
+                result = firstNumber * secNumber;
+                Console.WriteLine(result);
+            }
+            else if (option == "d")
+            {
+                result = firstNumber / secNumber;
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
