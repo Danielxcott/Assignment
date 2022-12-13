ed5ea3e61241396c5f322addaa222d809c1e7cdb
Thein Zan <changwilliam826@gmail.com>
2022-12-13 17:06:21 +0630

    still on processing tuto 8

---
+++ tutorial8/tutorial8/Default.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="tutorial8._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <h1>Create list of dogs</h1>
+    <div>
+        <asp:Label ID="NameLabel" runat="server" Text="Dog name"></asp:Label> <br />
+        <asp:TextBox ID="NameCreate" runat="server" CssClass="form-control"></asp:TextBox>
+        <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ControlToValidate="NameCreate" ErrorMessage="Required input field" ForeColor="Red"></asp:RequiredFieldValidator>
+        <asp:RegularExpressionValidator ID="CheckingMinLength" ControlToValidate="NameCreate" ValidationExpression="\[a-zA-Z]{3}" runat="server"  ForeColor="Red" ErrorMessage="Name must be at least in 3 lengths."></asp:RegularExpressionValidator>
+        <span runat="server" ID="Max" style="color: red"></span>
+        <div style="margin: 15px 0px">
+        <asp:Button ID="Button1" CssClass="btn btn-primary" runat="server" Text="Update" style="display:none" OnClick="Update_Btn" />
+        <asp:Button ID="CreateBtn" CssClass="btn btn-primary" style="margin-right:10px" runat="server" Text="Create" OnClick="Create_Btn"/>
+        <asp:Button ID="ClearBtn" CssClass="btn btn-danger" runat="server" Text="Clear" OnClick="Clear_Btn" />
+         
+        </div>
+    </div>
+    <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="false" DataKeyNames="id" OnRowEditing="GridView1_RowEditing" OnRowDeleting="GridView1_RowDeleting">  
+            <Columns>  
+                <asp:BoundField DataField="id" HeaderText="No." />  
+                <asp:BoundField DataField="name" HeaderText="Name" /> 
+                <asp:CommandField ShowEditButton="true" ControlStyle-CssClass="btn btn-warning" />  
+                <asp:CommandField ShowDeleteButton="true" ControlStyle-CssClass="btn btn-danger" /> 
+            </Columns>  
+    </asp:GridView>  
+</asp:Content>
---
+++ tutorial8/tutorial8/Default.aspx.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace tutorial8
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                ViewTable();
+            }
+        }
+
+        SqlConnection con = new SqlConnection(@"Data Source=THEINZAN-PC;Initial Catalog=Dogs;User ID=sa;Password=Password2254@;Pooling=False");
+
+        protected void Create_Btn(object sender, EventArgs e)
+        {
+            if(NameCreate.Text == String.Empty)
+            {
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Required input field.');", true);
+            }
+            else
+            {
+                    con.Open();
+                    SqlCommand check = new SqlCommand("Select name From [Dogs] Where name = @name",con);
+                    check.Parameters.AddWithValue("@name", NameCreate.Text.ToString());
+                    SqlDataAdapter sa = new SqlDataAdapter(check);
+                    DataTable dt = new DataTable();
+                    sa.Fill(dt);
+                    if (dt.Rows.Count > 0)
+                    {
+                        ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Name had already existed in the lists, write different name.');", true);
+                    }
+                    else
+                    {
+                        SqlCommand cmd = new SqlCommand("Insert into [Dogs] (name) values(@name)", con);
+                        cmd.Parameters.AddWithValue("@name", NameCreate.Text.ToString());
+                        cmd.ExecuteNonQuery();
+                        con.Close();
+                        Response.Redirect("Default.aspx");
+                        ViewTable();
+                    }
+                
+            }
+        }
+
+        protected void Clear_Btn(object sender, EventArgs e)
+        {
+            NameCreate.Text = string.Empty; 
+        }
+
+        protected void ViewTable()
+        {
+            SqlCommand cmd = new SqlCommand("Select * From [Dogs]",con);
+            DataTable dt = new DataTable();
+            SqlDataAdapter sa = new SqlDataAdapter(cmd);
+            sa.Fill(dt);
+            if (dt.Rows.Count > 0)
+            {
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+            }
+            else
+            {
+                DataRow dr = dt.NewRow();
+                dt.Rows.Add(dr);
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+            }
+        }
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            GridViewRow row = (GridViewRow)GridView1.Rows[e.RowIndex];
+            Label lbldeleteid = (Label)row.FindControl("lblID");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("DELETE FROM [Dogs] where id='" + Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Value.ToString()) + "'", con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            ViewTable();
+        }
+
+        protected void GridView1_RowEditing(object sender, GridViewDeleteEventArgs e)
+        {
+
+        }
+
+        protected void Update_Btn(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
