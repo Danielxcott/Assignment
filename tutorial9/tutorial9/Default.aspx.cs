9e9e81c33dbb4eecc0d782243a78204e9d88ee0b
Thein Zan <changwilliam826@gmail.com>
2022-12-15 10:32:24 +0630

    tutorial 9

---
+++ tutorial9/tutorial9/Default.aspx.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace tutorial9
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Upload_Btn(object sender, EventArgs e)
+        {
+            string fileExtension = Path.GetExtension(FileUpload1.PostedFile.FileName).ToLower();
+            string fileName = Path.GetFileName(FileUpload1.FileName);
+            string path = Server.MapPath("~/Files/") + fileName;
+            FileUpload1.PostedFile.SaveAs(path);
+            string filePaths = Path.GetFullPath(path);
+
+            switch (fileExtension)
+            {
+                case ".txt":
+                    getTxtTable(filePaths);
+                    break;
+                case ".xlsx":
+                    getXlsxTable(filePaths, "yes");
+                    break;
+                case ".csv":
+                    getCsvTable(filePaths);
+                    break;
+                default:
+                    ErrorMsg.Style.Add("display", "block");
+                    ErrorMsg.Text = "The file extension doesn't support.";
+                    ErrorMsg.ForeColor = System.Drawing.Color.Red;
+                    break;
+            }
+        }
+
+        //View txt file table
+        void getTxtTable(string path)
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("No");
+            table.Columns.Add("Product");
+            table.Columns.Add("Category");
+            table.Columns.Add("Quantity");
+            table.Columns.Add("Price");
+            table.Columns.Add("Status");
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] parts = sr.ReadLine().Split(',');
+                    table.Rows.Add(parts[0], parts[1], parts[2], parts[3], parts[4], parts[5]);
+                }
+            }
+            GridView1.DataSource = table;
+            GridView1.DataBind();
+        }
+
+        //View excel file table
+        void getXlsxTable(string path,string hdr)
+        {
+            string con = ConfigurationManager.ConnectionStrings["excelcon"].ConnectionString;
+            con = string.Format(con, path, hdr);
+            OleDbConnection excelcon = new OleDbConnection(con);
+            excelcon.Open();
+            DataTable exceldt = excelcon.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+            string excelSheetName = exceldt.Rows[0]["TABLE_NAME"].ToString();
+            OleDbCommand cmd = new OleDbCommand("Select * From [" + excelSheetName + "]", excelcon);
+            OleDbDataAdapter oda = new OleDbDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            oda.Fill(dt);
+            excelcon.Close();
+
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+        //View Csv file table
+        void getCsvTable(string path)
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("No");
+            table.Columns.Add("Name");
+            table.Columns.Add("Age");
+            table.Columns.Add("Occupation");
+            table.Columns.Add("Sport");
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] parts = sr.ReadLine().Split('|');
+                    table.Rows.Add(parts[0], parts[1], parts[2], parts[3], parts[4]);
+                }
+            }
+            GridView1.DataSource = table;
+            GridView1.DataBind();
+        }
+
+     }
+
+}       
+
+   
---
+++ tutorial9/tutorial9/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="tutorial9.Global" Language="C#" %>
